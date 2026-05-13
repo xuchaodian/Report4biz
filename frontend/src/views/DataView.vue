38f829080e047a3b1c0a3b1e4e050ca0bae0b7ea
xuchaodian <xuchaodian@192.168.1.27>
2026-05-13 17:55:21 +0800

    v1.6.1 恢复DataView批量购买按钮+组件导入
    之前代码回退导致BatchSmartstepsDialog导入和按钮丢失，现已恢复

--- frontend/src/views/DataView.vue
+++ frontend/src/views/DataView.vue
@@ -31,6 +31,13 @@
         >
           <el-icon><Delete /></el-icon>批量删除({{ selectedRows.length }})
         </el-button>
+        <el-button
+          v-if="selectedRows.length > 0"
+          type="primary"
+          @click="showBatchSmartstepsDialog = true"
+        >
+          <el-icon><DataAnalysis /></el-icon>批量购买({{ selectedRows.length }})
+        </el-button>
         <el-button type="danger" plain @click="handleClearAll">
           <el-icon><Delete /></el-icon>全清除
         </el-button>
@@ -547,6 +554,13 @@
       </template>
     </el-dialog>
 
+    <!-- 批量购买对话框 -->
+    <BatchSmartstepsDialog
+      :visible="showBatchSmartstepsDialog"
+      :stores="selectedRows"
+      @update:visible="showBatchSmartstepsDialog = $event"
+      @close="showBatchSmartstepsDialog = false"
+    />
   </div>
 </template>
 
@@ -554,6 +568,7 @@
 import { ref, reactive, computed, onMounted, watch, nextTick } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage, ElMessageBox } from 'element-plus'
+import BatchSmartstepsDialog from '@/components/BatchSmartstepsDialog.vue'
 import { Plus, Upload, Download, Search, Edit, Delete, Location, Close, MapLocation, DataAnalysis, TrendCharts, Loading } from '@element-plus/icons-vue'
 import axios from 'axios'
 import Papa from 'papaparse'
@@ -618,6 +633,7 @@ const uploadRef = ref(null)
 const uploadFile = ref(null)
 const tableRef = ref(null)
 const selectedRows = ref([])
+const showBatchSmartstepsDialog = ref(false)
 
 
 
